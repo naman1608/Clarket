9cb5ea2bd70a30c4792a6e01011f3f10c4230c44
naman1608 <1608naman@gmail.com>
2021-11-15 01:21:26 +0530

    restored

--- scripts/auth.js
+++ scripts/auth.js
@@ -24,6 +24,30 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+const loggedinelements = document.querySelectorAll('.logged-in');
+const loggedoutelements = document.querySelectorAll('.logged-out');
+
+const setupUI = (userCredential) => {
+    if(userCredential) {
+        console.log("setupui user");
+        console.log(userCredential);
+        const dropdownmenu = document.getElementById('dropdownMenuLink');
+        const curr = userCredential.displayName;
+        console.log(curr);
+        dropdownmenu.innerHTML = curr;
+        console.log("logged-in");
+        console.log(loggedinelements);
+        console.log(loggedoutelements);
+        loggedinelements.forEach(element => element.style.display = "block");
+        loggedoutelements.forEach(element => element.style.display = "none");
+    }
+    else {
+        console.log("logged-out");
+        loggedinelements.forEach(element => element.style.display = "none");
+        loggedoutelements.forEach(element => element.style.display = "block");
+    }
+}
+
 auth.onAuthStateChanged(user =>{
   setupUI(user);
 })
