9f3da93a34cc837da0765e967bc8497a1cae7c34
basedsugar <85169702+basedsugar@users.noreply.github.com>
2021-12-09 21:01:13 +0530

    smtp

--- signup.php
+++ signup.php
@@ -24,7 +24,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
             if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
-            redirect("signin.php");
+            // redirect("signin.php");
             } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
             
@@ -33,6 +33,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     else{
         echo "Passwords do not match";
     }
+}
+
+require 'PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+$mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'clarketnsut@gmail.com';                 // SMTP username
+$mail->Password = 'clarket@123';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('clarketnsut@gmail.com', 'Clarket');
+$mail->addAddress($_POST["emailid"], $_POST["username"]);     // Add a recipient               // Name is optional
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
 }
 
 
